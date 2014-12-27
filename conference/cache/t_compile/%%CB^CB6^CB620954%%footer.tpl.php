ecdf1af884f5aeecc844ce407bb977aba8f15a91
Hormetjan Yiltiz <hyiltiz@gmail.com>
2014-12-27 10:01:53 -0500

    keep labsite and conference seperate

--- conference/cache/t_compile/%%CB^CB6^CB620954%%footer.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2014-12-12 06:41:44
-         compiled from common/footer.tpl */ ?>
-<?php echo ''; ?><?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "core:common/footer.tpl", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?><?php echo ''; ?>
